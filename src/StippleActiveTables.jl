22cd2cd67ab8d57241e06d6ed05d89c18fb99f00
Helmut Hänsel <helmut.haensel@gmx.de>
2026-05-25 00:43:49 +0200

    ensure type stability of dataframe columns during parsing (except allowmissing)

--- src/StippleActiveTables.jl
+++ src/StippleActiveTables.jl
@@ -2,7 +2,6 @@ module StippleActiveTables
 
 using Stipple
 using Stipple: flexgrid_kwargs
-import Stipple.Genie
 import Stipple.Genie.Renderer.Html: normal_element, register_normal_element, script
 
 using DataFrames
@@ -12,40 +11,71 @@ export ActiveTable, activetable
 const assets_config = Genie.Assets.AssetsConfig(package = "StippleActiveTables.jl")
 
 function deps() :: Vector{String}
-    [script(type = "module", src = Genie.Assets.asset_path(assets_config, :js, file="activeTable.bundle"))]
+    [script(type = "module", src = Genie.Assets.asset_path(assets_config, :js, file = "activeTable.bundle"))]
 end
 
 function deps_routes(; basedir = @__DIR__) :: Nothing
     if !Genie.Assets.external_assets(assets_config)
-        Genie.Assets.add_fileroute(assets_config, "activeTable.bundle.js"; basedir = joinpath(dirname(basedir), "assets"))
+        Genie.Assets.add_fileroute(assets_config, "activeTable.bundle.js"; basedir = normpath(joinpath(@__DIR__, "..")))
     end
 
     nothing
-end
-
-function __init__()
-    deps_routes()
 end
 
 mutable struct ActiveTable
     data::DataFrame
 end
 
-function Stipple.stipple_parse(::Type{ActiveTable}, v::Vector)
-    isempty(v) && return DataFrame
-    matrix = reduce(hcat, v[2:end])
-    ActiveTable(DataFrame([[matrix[i, :]...] for i in 1:size(matrix, 1)], v[1]))
+register_normal_element("active__table", context = @__MODULE__)
+
+function activetable(data::Symbol, args...; kwargs...)
+    kwargs = Stipple.attributes(flexgrid_kwargs(; data, kwargs...))
+    active__table(args...; kwargs...)
 end
 
 function Stipple.render(at::ActiveTable)
     vcat([names(at.data)], [Matrix(at.data)[i, :] for i in 1:nrow(at.data)])
 end
 
-register_normal_element("active__table", context = @__MODULE__)
+function Stipple.stipple_parse(::Type{ActiveTable}, v::Vector)
+    ActiveTable(DataFrame(permutedims(reduce(hcat, v[2:end])), v[1]))
+end
 
-function activetable(data::Symbol, args...; kwargs...)
-    kwargs = Stipple.attributes(flexgrid_kwargs(; data, kwargs...))
-    active__table(:active__table, args...; kwargs...)
+function Stipple.convertvalue(activetable::Union{Ref{ActiveTable}, R{ActiveTable}}, v::Vector)
+    df_new = Stipple.stipple_parse(ActiveTable, v).data
+    df = activetable[].data
+    nn = names(df)
+    nn_new = names(df_new)
+    nn = intersect(nn, nn_new)
+    for n in nn
+        T = nonmissingtype(eltype(df[:, n]))
+        resize!(df, nrow(df_new))
+        for i in 1:nrow(df_new)
+            x = df_new[i, n]
+            df[i, n] = if T <: AbstractString
+                x
+            elseif x === missing || x == ""
+                allowmissing!(df, n)
+                missing
+            else
+                try
+                    Stipple.stipple_parse(T, x)
+                catch e
+                    @warn "Failed to parse value to type $T, make sure the value is compatible or modify the underlying DataFrame column type".
+                    missing
+                end
+            end
+        end
+    end
+    for n in setdiff(nn_new, nn)
+        df[!, n] = string.(df_new[:, n])
+    end
+    select!(df, nn_new)
+    activetable
+end
+
+function __init__()
+    deps_routes()
 end
 
 end # module StippleActiveTables
